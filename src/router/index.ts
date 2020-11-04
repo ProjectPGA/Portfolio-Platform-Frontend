1c9632d123f20bd4e97267acfc7bfb2dbdefaeff
afhg <andres.hernandez@star-group.net>
2020-11-04 16:30:48 +0100

    initial commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
+import Maintenance from '../views/Maintenance.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
         path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/maintenance',
+        name: 'Maintenance',
+        component: Maintenance,
     },
     {
         path: '/about',
---
+++ src/views/Maintenance.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="maintenance-page">
+        new change
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    name: 'Maintenance',
+})
+export default class Maintenance extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.maintenance-page {
+    background-color: red;
+    color: white;
+}
+</style>
