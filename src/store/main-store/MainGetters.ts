aaa366a273279eabdf6b94e1a781957f116f4b62
andresws <andreshernandez12.1995@gmail.com>
2020-11-17 12:59:46 +0100

    Change camel case from getters on main menu

--- src/store/main-store/MainGetters.ts
+++ src/store/main-store/MainGetters.ts
@@ -2,7 +2,7 @@ import { Getters } from 'vuex-smart-module';
 import MainState from './MainState';
 
 export default class MainGetters extends Getters<MainState> {
-    public get reversetestdata(): boolean {
+    public get reverseTestdata(): boolean {
         return !this.state.testData;
     }
 }
