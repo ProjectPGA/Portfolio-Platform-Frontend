f4db13575565c5f24e53e3678f0c5cfeb15a37a2
andresws <andreshernandez12.1995@gmail.com>
2021-01-31 14:51:16 +0100

    unit test > test data: Remove unnneccessary code

--- src/store/main-store/testData.ts
+++ src/store/main-store/testData.ts
@@ -1,13 +1,1 @@
 import { AskAnExpertState, getInitialState } from './store';
-import { AvailablePoliciesResponse } from '@/webservices/models/askAnExpert/AvailablePoliciesResponse';
-
-export const emptyState: AskAnExpertState = getInitialState();
-
-export const getAvailablePoliciesResponse: AvailablePoliciesResponse = {
-    policies: [
-        {
-            key: 'p000',
-            value: 'Policy 000',
-        },
-    ],
-};
